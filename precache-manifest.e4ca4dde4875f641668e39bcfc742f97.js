f320c7181b7a32103a64ce39686f93ac78554a9c
Lionel Tzatzkin <elrumordelaluz@hotmail.com>
2019-03-19 12:55:14 +0100

    Updates

---
+++ precache-manifest.e4ca4dde4875f641668e39bcfc742f97.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "58f2fad8a07d4f486c70c523b7c428c6",
+    "url": "/static/media/github.58f2fad8.svg"
+  },
+  {
+    "revision": "f4beecdf2028635a1bd6ba7a29b26551",
+    "url": "/static/media/codepen.f4beecdf.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "a29d73a1ae56f4224987",
+    "url": "/static/js/main.a29d73a1.chunk.js"
+  },
+  {
+    "revision": "86b997b6f12a079a5498",
+    "url": "/static/js/2.86b997b6.chunk.js"
+  },
+  {
+    "revision": "a29d73a1ae56f4224987",
+    "url": "/static/css/main.a98eaf24.chunk.css"
+  },
+  {
+    "revision": "ce9ecaf311520d549a35f55c6aff2e73",
+    "url": "/index.html"
+  }
+];
