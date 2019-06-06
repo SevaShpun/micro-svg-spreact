17c0261ff90c6cf4cfe866284cecf9137a026c60
Lionel Tzatzkin <elrumordelaluz@hotmail.com>
2019-06-06 15:05:14 +0200

    Updates

---
+++ precache-manifest.c350b39bf50b6c927ff2b140f85cc247.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "154ecc7dd566e7b91fb30c80acdfcfd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7e355dfd68bf7652315",
+    "url": "/static/css/main.eb9be260.chunk.css"
+  },
+  {
+    "revision": "3a79f1c90aeedd2a6563",
+    "url": "/static/js/2.cf7bea33.chunk.js"
+  },
+  {
+    "revision": "c7e355dfd68bf7652315",
+    "url": "/static/js/main.d76e9ded.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "f4beecdf2028635a1bd6ba7a29b26551",
+    "url": "/static/media/codepen.f4beecdf.svg"
+  },
+  {
+    "revision": "58f2fad8a07d4f486c70c523b7c428c6",
+    "url": "/static/media/github.58f2fad8.svg"
+  }
+]);
